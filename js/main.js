0e6bd3f188b8744fe7c559371bf1d5558e7b0a0e
Ryan Huber <ryan.huber@mac.com>
2014-08-13 15:50:04 -0400

    added additional strings to the quotes array

--- js/main.js
+++ js/main.js
@@ -11,4 +11,8 @@ var todaysMonth = todaysDate.getMonth();
 var todaysDay = todaysDate.getDate();
 
 //create empty variables for each month to hold the strings of quotes
-var janQuotes = ["1500: The Portuguese explorer Pedro Alvares Cabral searches the coast of Brazil and claims the region for Portugal."], febQuotes = [], marQuotes = [], aprQuotes = [], mayQuotes = [], junQuotes = [], julQuotes = [], augQuotes = [], sepQuotes = [], octQuotes = [], novQuotes = []; decQuotes = [];
+var janQuotes = [
+"1500: The Portuguese explorer Pedro Alvares Cabral searches the coast of Brazil and claims the region for Portugal.", "1905: After a six-month siege, Russians surrender Port Arthur to the Japanese.", "1521: Martin Luther is excommunicated from the Catholic Church."], febQuotes = [], marQuotes = [], aprQuotes = [], mayQuotes = [], junQuotes = [], julQuotes = [], augQuotes = [], sepQuotes = [], octQuotes = [], novQuotes = []; decQuotes = [];
+
+
+$(document).ready
