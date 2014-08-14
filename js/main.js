c4ec4a24e3b68e981e140b501a028cef5c0d955b
Ryan Huber <ryan.huber@mac.com>
2014-08-14 11:06:59 -0400

    added date functionality for quotes

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ function findQuote (todaysMonth, todaysDay) {
 
 //create empty variables for each month to hold the strings of quotes
 var janQuotes = [
-"1500: The Portuguese explorer Pedro Alvares Cabral searches the coast of Brazil and claims the region for Portugal.", "1905: After a six-month siege, Russians surrender Port Arthur to the Japanese.", "1521: Martin Luther is excommunicated from the Catholic Church."],
+["1500", "The Portuguese explorer Pedro Alvares Cabral searches the coast of Brazil and claims the region for Portugal."], ["1905", "After a six-month siege, Russians surrender Port Arthur to the Japanese."], ["1521",  "Martin Luther is excommunicated from the Catholic Church."],
 
 febQuotes = [],
 
