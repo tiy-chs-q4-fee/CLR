b565946c08fe5ca02dac1a60b6b452478981393a
Ryan Huber <ryan.huber@mac.com>
2014-08-14 10:46:12 -0400

    changed the format of the date on the homepage by creating string variables

--- js/main.js
+++ js/main.js
@@ -2,13 +2,85 @@
 var todaysDate = new Date();
 
 
-// Break down the date object into seperate elements (month and day)
-
-// First get the month and assign it to a new variable (called: todaysMonth)
-var todaysMonth = todaysDate.getMonth();
+// Break down the date object into seperate elements (month and day and year)
+// Get the year and assign it to a new variable (called: todaysYear)
+var todaysYear = todaysDate.getFullYear();
 
 // Next get the day of the month and assign it to a new variable (called: todaysDay)
 var todaysDay = todaysDate.getDate();
+
+// Get the month and assign it to a new variable (called: todaysMonth)
+// Get a month number from the todaysDate date object
+var todaysMonth = todaysDate.getMonth();
+// next create a function that takes that month number and converts it into a string with the month name
+function nameMonth (monthNumber){
+  switch (monthNumber) {
+  case 0:
+    return 'Jan';
+    break;
+
+  case 1:
+    return 'Feb';
+    break;
+
+  case 2:
+    return 'Mar';
+    break;
+
+  case 3:
+    return 'Apr';
+    break;
+
+  case 4:
+    return 'May';
+    break;
+
+  case 5:
+    return 'Jun';
+    break;
+
+  case 6:
+    return 'Jul';
+    break;
+
+  case 7:
+    return 'Aug';
+    break;
+
+  case 8:
+    return 'Sep';
+    break;
+
+  case 9:
+    return 'Oct';
+    break;
+
+  case 10:
+    return 'Nov';
+    break;
+
+  case 11:
+    return 'Dec';
+    break;
+
+  default:
+    statements_def
+    break;
+}
+
+};
+
+var thisMonthName = nameMonth(todaysMonth);
+
+var homePageDateDisplay = thisMonthName + " " + todaysDay + ", " + todaysYear;
+
+
+
+
+// create a varibale with the date set up to display on the homepage without the hours and minute
+
+
+
 
 // create a function to get the quote from the right month and date
 function findQuote (todaysMonth, todaysDay) {
@@ -19,27 +91,27 @@ function findQuote (todaysMonth, todaysDay) {
     break;
 
   case 1:
-    return (janQuotes[todaysDay -1]);
+    return (febQuotes[todaysDay -1]);
     break;
 
   case 2:
-    return (janQuotes[todaysDay -1]);
+    return (marQuotes[todaysDay -1]);
     break;
 
   case 3:
-    return (janQuotes[todaysDay -1]);
+    return (aprQuotes[todaysDay -1]);
     break;
 
   case 4:
-    return (janQuotes[todaysDay -1]);
+    return (mayQuotes[todaysDay -1]);
     break;
 
   case 5:
-    return (janQuotes[todaysDay -1]);
+    return (junQuotes[todaysDay -1]);
     break;
 
   case 6:
-    return (janQuotes[todaysDay -1]);
+    return (julQuotes[todaysDay -1]);
     break;
 
   case 7:
@@ -47,7 +119,19 @@ function findQuote (todaysMonth, todaysDay) {
     break;
 
   case 8:
-    return (augQuotes[todaysDay -1]);
+    return (sepQuotes[todaysDay -1]);
+    break;
+
+  case 9:
+    return (octQuotes[todaysDay -1]);
+    break;
+
+  case 10:
+    return (novQuotes[todaysDay -1]);
+    break;
+
+  case 11:
+    return (decQuotes[todaysDay -1]);
     break;
 
   default:
@@ -74,7 +158,7 @@ junQuotes = [],
 
 julQuotes = [],
 
-augQuotes = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "1521: Martin Luther is excommunicated from the Catholic Church.", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25", "Day 26", "Day 27", "Day 28", "Day 29", "Day 30", "Day 31" ],
+augQuotes = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "On this day in 1961, Berlin was divided as East Germany sealed off the border between the city's eastern and western sectors in order to halt the flight of refugees.", "Nothing bad happened on this day, your day is actually the worst in history!", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25", "Day 26", "Day 27", "Day 28", "Day 29", "Day 30", "Day 31" ],
 sepQuotes = [],
 
 octQuotes = [],
@@ -86,7 +170,7 @@ decQuotes = [];
 
 $(document).ready(function(){
 
-  $('.dateLine').html(todaysDate);
+  $('.dateLine').html(homePageDateDisplay);
 
 // Created a function to toggle the class replace on and off of the textBox
 
